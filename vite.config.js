8e214d2beb462c98c5b402618d086d7d4f6379ee
Jesse Huang <jeh027@ucsd.edu>
2024-05-28 00:01:26 -0700

    update interactive histogram

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,10 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [svelte()],
-  base: '/Tennis_Data_Viz/'
+  base: '/Tennis_Data_Viz/',
+  build: {
+    rollupOptions: {
+      external: ['d3'],
+    },
+  },
 })
