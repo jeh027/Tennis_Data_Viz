4bbdefa38ff9ab7d609ac41d271c47ceecdd6da0
Jesse Huang <jeh027@ucsd.edu>
2024-05-28 00:04:52 -0700

    back to normal

--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,5 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [svelte()],
-  base: '/Tennis_Data_Viz/',
-  build: {
-    rollupOptions: {
-      external: ['d3'],
-    },
-  },
+  base: '/Tennis_Data_Viz/'
 })
