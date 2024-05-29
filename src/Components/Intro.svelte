f2fc7ce5b44dbca63a1bbfb77bd01e81a8dffffc
Danny Lee <65679453+dannytylee@users.noreply.github.com>
2024-05-28 18:03:58 -0700

    changed title, subtitle, intro

--- src/Components/Intro.svelte
+++ src/Components/Intro.svelte
@@ -3,13 +3,7 @@
 
 <section>
   <p class="body-text">
-    Lorem ipsum dolor sit amet <a href="">random link</a> adipisicing elit. Esse
-    dolor nobis
-    <span class="bold">bold text</span> eius voluptate distinctio odit ut quod illo!
-    Nulla consequatur fugiat quia commodi aspernatur necessitatibus est doloremque
-    quam vitae dolorem. Vitae natus ipsum animi officiis et quis obcaecati in molestiae
-    aperiam, perferendis odio a ipsam nam suscipit laboriosam corporis excepturi
-    quisquam? Ratione delectus repellat error maxime quo beatae ullam, quod
+    Tennis is a sport deeply influenced by the surface on which it is played. The three primary court types—grass, clay, and hard—each bring unique characteristics that shape the game's dynamics and player performance. From the traditional elegance of grass courts to the gritty endurance required on clay, and the versatile resilience of hard courts, the history and evolution of these surfaces offer a fascinating glimpse into the sport's development. This article delves into the origins of these surfaces, explores how they affect the speed and bounce of the ball, and compares how players have adapted and excelled on each terrain, revealing the intricate interplay between surface and skill.
   </p>
   <p class="body-text">This is a trr</p>
 </section>
